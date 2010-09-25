3c4e3e30752d221a8d9e184c6a78ad7aadd78aa8
taichi <ryushi@gmail.com>
2010-09-26 00:50:04 +0900

    fix fileextesion bug.

--- org.handwerkszeug.gist/src/org/handwerkszeug/gist/job/GistJob.java
+++ org.handwerkszeug.gist/src/org/handwerkszeug/gist/job/GistJob.java
@@ -1,7 +1,9 @@
 package org.handwerkszeug.gist.job;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
@@ -28,10 +30,120 @@ import org.eclipse.ui.dialogs.PreferencesUtil;
 import org.eclipse.ui.progress.WorkbenchJob;
 import org.handwerkszeug.gist.Activator;
 import org.handwerkszeug.gist.Constants;
+import org.handwerkszeug.gist.util.LogUtil;
 import org.handwerkszeug.gist.util.StatusUtil;
 import org.handwerkszeug.gist.util.Streams;
+import org.handwerkszeug.gist.util.StringUtil;
 
 public class GistJob extends WorkspaceJob {
+
+	static final Set<String> extensions = new HashSet<String>();
+	static {
+		extensions.add(".txt");
+		extensions.add(".as");
+		extensions.add(".c");
+		extensions.add(".cs");
+		extensions.add(".cpp");
+		extensions.add(".css");
+		extensions.add(".cl");
+		extensions.add(".diff");
+		extensions.add(".hrl");
+		extensions.add(".html");
+		extensions.add(".hs");
+		extensions.add(".java");
+		extensions.add(".js");
+		extensions.add(".lua");
+		extensions.add(".m");
+		extensions.add(".php");
+		extensions.add(".pl");
+		extensions.add(".py");
+		extensions.add(".rb");
+		extensions.add(".sql");
+		extensions.add(".scala");
+		extensions.add(".sls");
+		extensions.add(".tex");
+		extensions.add(".xml");
+		extensions.add(".ascx");
+		extensions.add(".scpt");
+		extensions.add(".arc");
+		extensions.add(".asm");
+		extensions.add(".bat");
+		extensions.add(".befunge");
+		extensions.add(".boo");
+		extensions.add(".b");
+		extensions.add(".c-objdump");
+		extensions.add(".clj");
+		extensions.add(".coffee");
+		extensions.add(".cfm");
+		extensions.add(".cppobjdump");
+		extensions.add(".feature");
+		extensions.add(".d");
+		extensions.add(".d  -objdump");
+		extensions.add(".darcspatch");
+		extensions.add(".pas");
+		extensions.add(".dylan");
+		extensions.add(".e");
+		extensions.add(".fs");
+		extensions.add(".f");
+		extensions.add(".s");
+		extensions.add(".kid");
+		extensions.add(".po");
+		extensions.add(".go");
+		extensions.add(".man");
+		extensions.add(".groovy");
+		extensions.add(".mustache");
+		extensions.add(".erb");
+		extensions.add(".phtml");
+		extensions.add(".hx");
+		extensions.add(".haml");
+		extensions.add(".ini");
+		extensions.add(".weechatlog");
+		extensions.add(".io");
+		extensions.add(".jsp");
+		extensions.add(".ll");
+		extensions.add(".lhs");
+		extensions.add(".mak");
+		extensions.add(".mao");
+		extensions.add(".mkd");
+		extensions.add(".matlab");
+		extensions.add(".mxt");
+		extensions.add(".md");
+		extensions.add(".moo");
+		extensions.add(".myt");
+		extensions.add(".numpy");
+		extensions.add(".objdump");
+		extensions.add(".j");
+		extensions.add(".pir");
+		extensions.add(".pd");
+		extensions.add(".pytb");
+		extensions.add(".r");
+		extensions.add(".rhtml");
+		extensions.add(".rkt");
+		extensions.add(".raw");
+		extensions.add(".rebol");
+		extensions.add(".cw");
+		extensions.add(".sass");
+		extensions.add(".self");
+		extensions.add(".sh");
+		extensions.add(".st");
+		extensions.add(".tpl");
+		extensions.add(".sc");
+		extensions.add(".tcl");
+		extensions.add(".tcsh");
+		extensions.add(".txt");
+		extensions.add(".textile");
+		extensions.add(".vhdl");
+		extensions.add(".vala");
+		extensions.add(".v");
+		extensions.add(".vim");
+		extensions.add(".bas");
+		extensions.add(".yml");
+		extensions.add(".mu");
+		extensions.add(".ooc");
+		extensions.add(".rst");
+	}
+
+	static final String MAYBE_ERROR_URL = "http://gist.github.com/gists/new";
 
 	protected IFile file;
 	protected String txt;
@@ -51,7 +163,7 @@ public class GistJob extends WorkspaceJob {
 		monitor.beginTask("in progress...", 2);
 		try {
 			final String resultUri = post(monitor);
-			if (resultUri != null) {
+			if (StringUtil.isEmpty(resultUri) == false) {
 				new WorkbenchJob("copy to clipboard...") {
 					@Override
 					public IStatus runInUIThread(IProgressMonitor monitor) {
@@ -116,8 +228,8 @@ public class GistJob extends WorkspaceJob {
 			if (this.isPrivate) {
 				nvs.add(new BasicNameValuePair("action_button", "private"));
 			}
-			nvs.add(new BasicNameValuePair("file_ext[gistfile1]", this.file
-					.getFileExtension()));
+			nvs.add(new BasicNameValuePair("file_ext[gistfile1]",
+					getExtension()));
 			nvs.add(new BasicNameValuePair("file_name[gistfile1]", this.file
 					.getName()));
 			nvs.add(new BasicNameValuePair("file_contents[gistfile1]", getTxt()));
@@ -130,7 +242,13 @@ public class GistJob extends WorkspaceJob {
 			StatusLine line = response.getStatusLine();
 			if (line.getStatusCode() == 302) { // Found
 				Header h = response.getFirstHeader("Location");
-				result = h.getValue();
+				String location = h.getValue();
+				if (MAYBE_ERROR_URL.equals(location)) {
+					nvs.remove(1);// token
+					LogUtil.log(Activator.getDefault(), nvs.toString());
+				} else {
+					result = location;
+				}
 			} else if (line.getStatusCode() == 401) { // Unauthorized
 				throw new UnauthorizedException();
 			}
@@ -145,10 +263,22 @@ public class GistJob extends WorkspaceJob {
 	}
 
 	protected String getTxt() throws CoreException {
-		if (this.txt != null) {
+		if (StringUtil.isEmpty(this.txt)) {
+			return Streams.readText(this.file.getContents());
+		} else {
 			return this.txt;
 		}
-		return Streams.readText(this.file.getContents());
+	}
+
+	protected String getExtension() {
+		String ext = this.file.getFileExtension();
+		if (StringUtil.isEmpty(ext) == false) {
+			ext = "." + ext.trim().toLowerCase();
+			if (extensions.contains(ext)) {
+				return ext;
+			}
+		}
+		return ".txt";
 	}
 
 	protected String getLogin() {
